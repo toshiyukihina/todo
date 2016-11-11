97546f3f09ba1b3c024518a9b53c2b5390d27d3a
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-11-11 10:53:51 +0900

    introduce redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
